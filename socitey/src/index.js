e39d577254691a3add451c1ba86518bf283a9708
Girish Ravindra Shinde <shindegirish186@gmail.com>
2024-12-27 00:03:49 +0530

    install jquery
    install jquery using following command:-
    
    npm install jquery

--- socitey/src/index.js
+++ socitey/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+// import "bootstrap/dist/js/bootstrap.min.js";
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Includes Bootstrap JavaScript and Popper.js
 import "./css/style.css";
 import projectRoute from './projectRoute';
 import { RouterProvider } from 'react-router-dom';
@@ -9,6 +10,9 @@ import { RouterProvider } from 'react-router-dom';
 import "https://code.jquery.com/jquery-3.5.1.slim.min.js"
 import "https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
 import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+
+import $ from 'jquery'; // Import jQuery
+window.$ = $; // Make jQuery globally accessible if needed
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
