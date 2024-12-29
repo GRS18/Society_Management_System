2591f33221dfdcb0b804b1a8aba362f40390600c
ChaudhariNikita <nikitachaudhari45800@gmail.com>
2024-12-29 19:32:13 +0530

    update about us page

--- socitey/src/components/AboutUs.js
+++ socitey/src/components/AboutUs.js
@@ -4,11 +4,13 @@ import logo from "../assets/logo.png";
 import Socitey from "../assets/Socitey.jpg";
 import profile from "../assets/profile.png";
 import profile1 from "../assets/profile1.png";
+import aboutus from "../assets/about.png";
 import "../css/aboutus.css";
 
 export default function AboutUs() {
   return (
     <>
+
       {/* Header Start */}
       <div className="container-fluid">
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -52,15 +54,15 @@ export default function AboutUs() {
                     Contact us
                   </Link>
                 </li>
-                 <li className="nav-item">
-                    <Link
-                      className="nav-link active header-button d-sm-inline-block"                       
-                      aria-current="page"
-                      to="/login">
-                      Login      
-                      </Link>
-                  </li>
-               {/* <li className="nav-item ">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active header-button d-sm-inline-block"
+                    aria-current="page"
+                    to="/login">
+                    Login
+                  </Link>
+                </li>
+                {/* <li className="nav-item ">
                   <Link className="nav-link active header-button d-sm-inline-block" aria-current="page" to="/register">
                    Register
                   </Link>
@@ -72,15 +74,29 @@ export default function AboutUs() {
       </div>
       {/* Header End */}
 
-      <section className="about-header">
-        <div className="container">
-          <h1>About Us</h1>
-          <p>
-            Learn more about our society and how we strive to create a better
-            living experience for all residents.
-          </p>
+      <section class="page-header bg-light py-5 about-header">
+        <div class="container text-center">
+          <div class="page-header-content">
+            <h1>About Us</h1>
+            <p>
+              Learn more about our society and how we strive to create a better
+              living experience for all residents.
+            </p>
+            <ul class="breadcrumb list-inline mt-2">
+              <li class="list-inline-item">
+                <Link to="/" class="text-secondary text-decoration-none">Home</Link>
+              </li>
+              <li class="list-inline-item text-secondary">
+                &rarr;
+              </li>
+              <li class="list-inline-item text-dark">
+                About Us
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
+
 
       <section className="about py-5">
         <div className="container">
@@ -88,13 +104,13 @@ export default function AboutUs() {
             <div className="col-md-6">
               <h2>Overview</h2>
               <p className="overview_para">
-              Rohan Builders started in 1993, today has a team of over 2,000+ professionals
-              and landmarks sprawling over 1.5 crore square feet across the country. 
-               It is consistently rated DA2+ by CRISIL, signifying the Group’s ability to 
-               execute projects as per specified quality, within timelines. All Rohan projects, residential, 
-               commercial or infrastructure, are characterized by innovative design, attention to detail, and modern 
-               conveniences, catering to the evolving needs and preferences of homebuyers and businesses. As a participant at the UNGC, 
-               the Group stays dedicated in its sustainability efforts. It has also been certified as a Great Place to Work by the renowned GPTW organization.
+                Rohan Builders started in 1993, today has a team of over 2,000+ professionals
+                and landmarks sprawling over 1.5 crore square feet across the country.
+                It is consistently rated DA2+ by CRISIL, signifying the Group’s ability to
+                execute projects as per specified quality, within timelines. All Rohan projects, residential,
+                commercial or infrastructure, are characterized by innovative design, attention to detail, and modern
+                conveniences, catering to the evolving needs and preferences of homebuyers and businesses. As a participant at the UNGC,
+                the Group stays dedicated in its sustainability efforts. It has also been certified as a Great Place to Work by the renowned GPTW organization.
               </p>
             </div>
             <div className="col-md-6">
@@ -114,18 +130,18 @@ export default function AboutUs() {
             <div className="col-md-6">
               <h3>Achievements</h3>
               <p className="achievement">
-              Rohan Group’s excellence in design and innovation is weaved into the nation’s 
-              infrastructure – the Amritsar Wagah NH1 that connects India to Pakistan, and other roads, 
-              tunnels, bridges across Punjab, Rajasthan, Maharashtra. More than 50% of our clients are celebrated
-               brands and businesses and Multi-National Corporations.
+                Rohan Group’s excellence in design and innovation is weaved into the nation’s
+                infrastructure – the Amritsar Wagah NH1 that connects India to Pakistan, and other roads,
+                tunnels, bridges across Punjab, Rajasthan, Maharashtra. More than 50% of our clients are celebrated
+                brands and businesses and Multi-National Corporations.
               </p>
             </div>
             <div className="col-md-6">
               <h3>Best place to work</h3>
               <p className="place_work">
-              Rohan Group encourages both innovation and accountability by giving freedom within boundaries, 
-              making it one of the finest places to work. With feet firmly on ground and vision far ahead in the 
-              future, Rohan Group continues to inspire and lead the industry.
+                Rohan Group encourages both innovation and accountability by giving freedom within boundaries,
+                making it one of the finest places to work. With feet firmly on ground and vision far ahead in the
+                future, Rohan Group continues to inspire and lead the industry.
               </p>
             </div>
           </div>
@@ -154,6 +170,10 @@ export default function AboutUs() {
           </div>
         </div>
       </section>
+      <div className="container image-container">
+        <img src={aboutus} alt="about us" />
+      </div>
+
     </>
   );
 }
